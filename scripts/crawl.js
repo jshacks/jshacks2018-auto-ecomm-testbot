be166c7703a932fcb93f9473cad890a1f044a401
Alexandru Albu <afalex89@gmail.com>
2018-10-27 18:04:05 +0300

    updated crawler

--- scripts/crawl.js
+++ scripts/crawl.js
@@ -8,13 +8,18 @@ const css        = require('./cssStats');
 const websites = require('./../data/input_websites');
 
 chromeLauncher.launch({chromeFlags: ['--headless']}).then(chrome => {
-  for (const website of websites.slice(0, 1)) {
-    lighthouse(website.slug, website.main_url, chrome).then(result => {
-      fs.writeFile(path.resolve(__dirname, `./../data/lighthouse/${website.slug}.json`), JSON.stringify(result));
+  for (const website of [{
+    "slug": "emag",
+    "base_url": "emag.ro",
+    "main_url": "https://www.emag.ro/homepage",
+    "category": "Retail"
+  }]) {
+    Promise.all([
+      lighthouse(website.slug, website.main_url, chrome),
+      css(website.slug, website.main_url),
+    ]).then(results => {
+      fs.writeFile(path.resolve(__dirname, `./../data/lighthouse/${website.slug}.json`), JSON.stringify(results[0]));
+      fs.writeFile(path.resolve(__dirname, `./../data/css/${website.slug}.json`), JSON.stringify(results[1]));
     });
-    
-    css(website.main_url).then(result => {
-      fs.writeFile(path.resolve(__dirname, `./../data/css/${website.slug}.json`), JSON.stringify(result));
-    })
   }
 });
--- scripts/cssStats.js
+++ scripts/cssStats.js
@@ -1,11 +1,19 @@
 const cssStats = require('cssstats');
 
-module.exports = async (url) => {
-  let css = await cssStats(url);
+module.exports = function runCss(slug, url) {
+  console.log(`-- Start css crawl for ${slug}`);
   
-  return cssStats(css.css, {
-    specificityGraph : true,
-    repeatedSelectors: true,
-    propertyResets   : true,
+  const promise = new Promise((resolve, reject) => {
+    const stats = cssStats(url, {
+      specificityGraph : true,
+      repeatedSelectors: true,
+      propertyResets   : true,
+    });
+  
+    console.log(`-- Done css crawl for ${slug}`);
+  
+    resolve(stats);
   });
+  
+  return promise;
 };
