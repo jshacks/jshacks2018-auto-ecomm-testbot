f773b11b0870ddc8d1baf8d67dcfc0bb2b64fa5d
Alexandru Albu <afalex89@gmail.com>
2018-10-27 18:39:50 +0300

    updated crawler

--- scripts/crawl.js
+++ scripts/crawl.js
@@ -8,18 +8,31 @@ const css        = require('./cssStats');
 const websites = require('./../data/input_websites');
 
 chromeLauncher.launch({chromeFlags: ['--headless']}).then(chrome => {
-  for (const website of [{
-    "slug": "emag",
-    "base_url": "emag.ro",
-    "main_url": "https://www.emag.ro/homepage",
-    "category": "Retail"
-  }]) {
-    Promise.all([
-      lighthouse(website.slug, website.main_url, chrome),
-      css(website.slug, website.main_url),
-    ]).then(results => {
-      fs.writeFile(path.resolve(__dirname, `./../data/lighthouse/${website.slug}.json`), JSON.stringify(results[0]),function() {});
-      fs.writeFile(path.resolve(__dirname, `./../data/css/${website.slug}.json`), JSON.stringify(results[1]),function() {});
+  let promises = [];
+  for (const website of websites.splice(0, 1)) {
+    let promise = new Promise((resolve, reject) => {
+      Promise.all([
+        lighthouse(website.slug, website.main_url, chrome),
+        css(website.slug, website.main_url),
+      ]).then(results => {
+        resolve({
+          slug      : website.slug,
+          lighthouse: results[0],
+          css       : results[1],
+        });
+      });
     });
+    
+    promises.push(promise);
   }
+  
+  Promise.all(promises).then(websites => {
+    for (const websiteResults of websites) {
+      fs.writeFile(path.resolve(__dirname, `./../data/lighthouse/${websiteResults.slug}.json`), JSON.stringify(websiteResults.lighthouse));
+      fs.writeFile(path.resolve(__dirname, `./../data/css/${websiteResults.slug}.json`), JSON.stringify(websiteResults.css));
+    }
+  }).then(() => {
+    chrome.kill();
+    return;
+  });
 });
