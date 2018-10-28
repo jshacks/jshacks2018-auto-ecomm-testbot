13114f41d5874700ff40ef44de08d51fd722ca98
Alexandru Albu <afalex89@gmail.com>
2018-10-28 14:28:31 +0200

    added tensorflow

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 const tf   = require('@tensorflow/tfjs');
 const fs   = require('fs');
 const path = require('path');
+const _    = require('lodash');
 
 require('@tensorflow/tfjs-node');
 
-const performanceTrainData = JSON.parse(fs.readFileSync('./data/performance_train.json', 'utf8'));
+const uiTrainData   = JSON.parse(fs.readFileSync('./data/ui_train_data.json', 'utf8'));
+const uiTrainLabels = JSON.parse(fs.readFileSync('./data/ui_train_labels.json', 'utf8'));
 
-const performanceXs = tf.tensor2d(performanceTrainData.slice(0, 5));
-const performanceYs = tf.tensor2d(performanceTrainData.slice(5, performanceTrainData.length));
+const uiTestData   = JSON.parse(fs.readFileSync('./data/ui_test_data.json', 'utf8'));
+const uiTestLabels = JSON.parse(fs.readFileSync('./data/ui_test_labels.json', 'utf8'));
+
+const xs = tf.tensor(uiTrainData);
+const ys = tf.tensor(_.flatten(uiTrainLabels));
+
+const predict       = tf.tensor(uiTestData);
+const predictLabels = _.flatten(uiTestLabels);
+
+console.log(`--> Xs`);
+xs.print();
+
+console.log(`--> Ys`);
+ys.print();
 
 const model = tf.sequential();
 
 // Two layers with dropout between
-model.add(tf.layers.dense({units: 10, activation: 'relu'}));
+model.add(tf.layers.dense({units: 5, inputShape: 5, activation: 'relu'}));
 model.add(tf.layers.dropout({rate: 0.01}));
-model.add(tf.layers.dense({units: 5, activation: 'relu'}));
+model.add(tf.layers.dense({units: 1, activation: 'relu'}));
 
 // 5 classes for each test value
-model.add(tf.layers.dense({units: 5, activation: 'softmax'}));
+// model.add(tf.layers.dense({units: 5, activation: 'softmax'}));
 
-performance.print();
+model.compile({optimizer: 'sgd', loss: 'binaryCrossentropy', lr: 0.01});
+
+model.fit(xs, ys, {
+  batchSize: 1,
+  epochs   : 20,
+}).then(() => {
+  console.log('--> Prediction');
+  
+  return model.predict(predict).data();
+}).then((predictionsData) => {
+  for (let [index, label] of predictLabels.entries()) {
+    console.log(`--> Prediction #${index + 1}: ${predictionsData[index]} (${label})`);
+  }
+}).catch(error => {
+  console.error(error);
+});
+
