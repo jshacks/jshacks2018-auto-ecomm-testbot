c671da5825d373d0ef99f228d4a43ab333e557dc
Alexandru Albu <afalex89@gmail.com>
2018-10-28 16:02:37 +0200

    update tensorflow

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const uiTestData   = JSON.parse(fs.readFileSync('./data/ui_test_data.json', 'utf
 const uiTestLabels = JSON.parse(fs.readFileSync('./data/ui_test_labels.json', 'utf8'));
 
 const xs = tf.tensor(uiTrainData);
-const ys = tf.tensor(_.flatten(uiTrainLabels));
+// const ys = tf.tensor(uiTrainLabels);
+// const ys = tf.tensor(_.flatten(uiTrainLabels));
+const ys = tf.squeeze(tf.oneHot(tf.tensor(uiTrainLabels, undefined, 'int32'), 5));
 
 const predict       = tf.tensor(uiTestData);
 const predictLabels = _.flatten(uiTestLabels);
@@ -27,25 +29,27 @@ const model = tf.sequential();
 
 // Two layers with dropout between
 model.add(tf.layers.dense({units: 5, inputShape: 5, activation: 'relu'}));
-model.add(tf.layers.dropout({rate: 0.01}));
-model.add(tf.layers.dense({units: 1, activation: 'relu'}));
+model.add(tf.layers.dropout({rate: 0.3}));
 
 // 5 classes for each test value
-// model.add(tf.layers.dense({units: 5, activation: 'softmax'}));
+model.add(tf.layers.dense({units: 5, activation: 'softmax'}));
 
-model.compile({optimizer: 'sgd', loss: 'binaryCrossentropy', lr: 0.01});
+model.compile({optimizer: 'rmsprop', loss: 'categoricalCrossentropy', lr: 0.001, metrics: ['accuracy']});
+
+console.log(xs.shape);
+console.log(ys.shape);
 
 model.fit(xs, ys, {
-  batchSize: 1,
-  epochs   : 20,
+  batchSize: 500,
+  epochs   : 5,
 }).then(() => {
-  console.log('--> Prediction');
+  // console.log('--> Prediction');
   
   return model.predict(predict).data();
 }).then((predictionsData) => {
-  for (let [index, label] of predictLabels.entries()) {
-    console.log(`--> Prediction #${index + 1}: ${predictionsData[index]} (${label})`);
-  }
+  // for (let [index, label] of predictLabels.entries()) {
+  //   console.log(`--> Prediction #${index + 1}: ${predictionsData[index]} (${label})`);
+  // }
 }).catch(error => {
   console.error(error);
 });
